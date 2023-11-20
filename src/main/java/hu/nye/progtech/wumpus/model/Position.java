7cbfe4ec75357ff03017bef6527e5b556a890a24
Dandomate <dandomate@gmail.com>
2023-11-20 18:25:11 +0100

    Lépés kidolgozva, nyil kilövés müködik, van rajta még mit javitani de a funkciok müködnek, azért egybe rakom fel mert elfelejtettem commitolni...

---
+++ src/main/java/hu/nye/progtech/wumpus/model/Position.java
@@ -0,0 +1,74 @@
+package hu.nye.progtech.wumpus.model;
+
+public class Position {
+    private int row;
+    private char column;
+
+    public Position(int row, char column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public Position() {
+    }
+
+
+    public char getColumn() {
+        return column;
+    }
+
+    public void setColumn(char column) {
+        this.column = column;
+    }
+
+    public void moveUp() {
+        row--;
+    }
+
+    public void moveDown() {
+        row++;
+    }
+
+    public void moveLeft() {
+        column--;
+    }
+
+    public void moveRight() {
+        column++;
+    }
+
+    // Módosított toString metódus
+    public String toString() {
+        return "(" + row + ", " + column + ")";
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    //proba
+    public Position copy() {
+        return new Position(row, column);
+    }
+    public void moveInDirection(Direction direction) {
+        switch (direction) {
+            case NORTH:
+                row--;
+                break;
+            case WEST:
+                column--;
+                break;
+            case SOUTH:
+                row++;
+                break;
+            case EAST:
+                column++;
+                break;
+        }
+    }
+
+}
